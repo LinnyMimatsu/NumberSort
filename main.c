fff610dd89f8c7df59d2e29ac330358474e12488
linn <dapressler@gmail.com>
2025-09-13 02:04:49 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * Name: Dom
+ * Description: Reads in a set amount of numbers given by the user and prints them in the original order
+ * and the sorted order
+ * Date: 09/13/25
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+    return 0;
+}
